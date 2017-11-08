d3d5ba7b272258ce9e94e84e4f8d0c2ca5998bf4
Michael Rans <rans@email.com>
2017-11-08 17:12:07 +0100

    Fix UNESCO tests

--- run.py
+++ run.py
@@ -24,17 +24,19 @@ def main():
     """Generate dataset and create it in HDX"""
 
     base_url = Configuration.read()['base_url']
-    downloader = Download(extra_params_yaml=join(expanduser("~"), 'unesco.yml'))
-    countriesdata = get_countriesdata(base_url, downloader)
-    logger.info('Number of datasets to upload: %d' % len(countriesdata))
-    for countrydata in countriesdata:
-        dataset, showcase = generate_dataset_and_showcase(base_url, downloader, countrydata)
-        if dataset:
-            dataset.update_from_yaml()
-            dataset.create_in_hdx()
-            showcase.create_in_hdx()
-            showcase.add_dataset(dataset)
+    with Download(extra_params_yaml=join(expanduser("~"), 'unesco.yml')) as downloader:
+        endpoints = Configuration.read()['endpoints']
+        countriesdata = get_countriesdata(base_url, downloader)
+        logger.info('Number of datasets to upload: %d' % len(countriesdata))
+        for countrydata in countriesdata:
+            dataset, showcase = generate_dataset_and_showcase(base_url, downloader, countrydata, endpoints)
+            if dataset:
+                dataset.update_from_yaml()
+                dataset.create_in_hdx()
+                showcase.create_in_hdx()
+                showcase.add_dataset(dataset)
+
 
 if __name__ == '__main__':
-    facade(main, hdx_site='test', project_config_yaml=join('config', 'project_configuration.yml'))
+    facade(main, hdx_site='feature', project_config_yaml=join('config', 'project_configuration.yml'))
 
--- unesco.py
+++ unesco.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 UNESCO:
-------------
+------
 
 Reads UNESCO API and creates datasets.
 
@@ -11,56 +11,112 @@ Reads UNESCO API and creates datasets.
 import logging
 
 from hdx.data.dataset import Dataset
-from hdx.data.hdxobject import HDXError
 from hdx.data.showcase import Showcase
-from hdx.utilities.location import Location
+from hdx.location.country import Country
 from slugify import slugify
 
 logger = logging.getLogger(__name__)
 
 
 def get_countriesdata(base_url, downloader):
-    response = downloader.download('%scodelist/UNESCO/CL_AREA/latest?format=sdmx-json&locale=en' % base_url)
+    response = downloader.download('%scodelist/UNESCO/CL_AREA/latest?format=sdmx-json' % base_url)
     jsonresponse = response.json()
-    return jsonresponse['codelist']['items']
+    return jsonresponse['Codelist'][0]['items']
 
 
-def generate_dataset_and_showcase(base_url, downloader, countrydata):
-    """Parse json of the form:
-    {
-    },
+def generate_dataset_and_showcase(base_url, downloader, countrydata, endpoints):
     """
-    title = '%s - Economic and Social Indicators' % countrydata['name'] #  Example title. Include country, but not organisation name in title!
-    logger.info('Creating dataset: %s' % title)
-    name = 'Organisation indicators for %s' % countrydata['name']  #  Example name which should be unique so can include organisation name and country
+    https://api.uis.unesco.org/sdmx/data/UNESCO,DEM_ECO,1.0/....AU.?format=csv-:-tab-true-y&locale=en&subscription-key=...
+    """
+    countryiso2 = countrydata['id']
+    countryname = countrydata['names'][0]['value']
+    countryiso3 = Country.get_iso3_from_iso2(countryiso2)
+    if countryiso3 is None:
+        countryiso3, _ = Country.get_iso3_country_code_fuzzy(countryname)
+        logger.info('Matched %s to %s!' % (countryname, countryiso3))
+    name = 'UNESCO indicators for %s' % countryname
     slugified_name = slugify(name).lower()
-    ...
+
+    title = '%s - Sustainable development, Education, Demographic and Socioeconomic Indicators' % countryname
     dataset = Dataset({
         'name': slugified_name,
-        'title': title,
-        ...
+        'title': title
     })
-    dataset.set_maintainer()
-    dataset.set_organization()
-    dataset.set_dataset_date()
-    dataset.set_expected_update_frequency()
-    dataset.add_country_location()
-    dataset.add_tags([])
+    dataset.set_maintainer('196196be-6037-4488-8b71-d786adf4c081')
+    dataset.set_organization('18f2d467-dcf8-4b7e-bffa-b3c338ba3a7c')
+    dataset.add_country_location(countryiso3)
+    logger.info('Creating dataset: %s' % title)
+    dataset.set_expected_update_frequency('Every year')
+    tags = ['indicators', 'UNESCO', 'sustainable development', 'demographic', 'socioeconomic', 'education']
+    dataset.add_tags(tags)
 
-    resource = {
-        'name': title,
-        'format': ,
-        'url': ,
-        'description':
-    }
-    dataset.add_update_resource(resource)
+    earliest_year = 10000
+    latest_year = 0
+    for endpoint in sorted(endpoints):
+        datastructure_url = '%sdataflow/UNESCO/%s/latest?references=datastructure&format=sdmx-json' % (base_url, endpoint)
+        response = downloader.download(datastructure_url)
+        json = response.json()
+        indicator = json['Dataflow'][0]['names'][0]['value']
+        dimensions = json['DataStructure'][0]['dimensionList']['dimensions']
+        urllist = ['%sdata/UNESCO,%s,1.0/' % (base_url, endpoint)]
+        for dimension in dimensions:
+            if dimension['id'] == 'REF_AREA':
+                urllist.append(countryiso2)
+            else:
+                urllist.append('.')
+        urllist.append('?format=sdmx-json&detail=structureonly&includeMetrics=true')
+        structure_url = ''.join(urllist)
+        response = downloader.download(structure_url)
+        json = response.json()
+        observations = json['structure']['dimensions']['observation']
+        time_periods = dict()
+        for observation in observations:
+            if observation['id'] == 'TIME_PERIOD':
+                for value in observation['values']:
+                    time_periods[int(value['id'])] = value['actualObs']
+        years = sorted(time_periods.keys(), reverse=True)
+        end_year = years[0]
+        if years[-1] < earliest_year:
+            earliest_year = years[-1]
+        if end_year > latest_year:
+            latest_year = end_year
+        urllist[-1] = '?format=csv'
+        urllist.append('')
+
+        description = endpoints[endpoint]
+        if description != ' ':
+            description = '[More information](%s)' % description
+
+        obs_count = 0
+        start_year = end_year
+
+        def create_resource():
+            urllist[-1] = '&startPeriod=%d&endPeriod=%d' % (start_year, end_year)
+            resource = {
+                'name': '%s (%d-%d)' % (indicator, start_year, end_year),
+                'description': description,
+                'format': 'csv',
+                'url': downloader.get_full_url(''.join(urllist))
+            }
+            dataset.add_update_resource(resource)
+
+        for year in years:
+            obs_count += time_periods[year]
+            if obs_count < 2000:
+                start_year = year
+                continue
+            obs_count = time_periods[year]
+            create_resource()
+            end_year = year
+        create_resource()
+    dataset.set_dataset_year_range(earliest_year, latest_year)
 
     showcase = Showcase({
         'name': '%s-showcase' % slugified_name,
-        'title':
-        'notes':
-        'url':
-        'image_url':
+        'title': 'Indicators for %s' % countryname,
+        'notes': 'Education, literacy and other indicators for %s' % countryname,
+        'url': 'http://uis.unesco.org/en/country/%s' % countryiso2,
+        'image_url': 'http://www.tellmaps.com/uis/internal/assets/uisheader-en.png'
     })
-    showcase.add_tags([])
+    showcase.add_tags(tags)
     return dataset, showcase
