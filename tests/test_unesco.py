01185326de5f8f041618ff5ae538880ca2ee6ba1
Mike <rans@email.com>
2019-09-04 16:56:49 +0200

    Fix tests

--- tests/test_unesco.py
+++ tests/test_unesco.py
@@ -8,9 +8,12 @@ from os.path import join
 from pprint import pprint
 
 import pytest
+from hdx.data.vocabulary import Vocabulary
 from hdx.hdx_configuration import Configuration
 from hdx.hdx_locations import Locations
 from hdx.location.country import Country
+from hdx.utilities.path import temp_dir
+import hdx.utilities.downloader
 
 from tests.testing_data import countrydata, dimensions, observations
 from unesco import generate_dataset_and_showcase, get_countriesdata, get_endpoints_metadata
@@ -23,15 +26,25 @@ class TestUnesco:
                               project_config_yaml=join('tests', 'config', 'project_configuration.yml'))
         Locations.set_validlocations([{'name': 'arg', 'title': 'Argentina'}])  # add locations used in tests
         Country.countriesdata(use_live=False)
+        Vocabulary._tags_dict = True
+        Vocabulary._approved_vocabulary = {'tags': [{'name': 'sustainable development'}, {'name': 'demographics'}, {'name': 'socioeconomics'}, {'name': 'education'}], 'id': '4e61d464-4943-4e97-973a-84673c1aaa87', 'name': 'approved'}
+
+    @pytest.fixture(scope='function')
+    def csv_content(self):
+        r = hdx.utilities.downloader.Download(user_agent='test').download('https://raw.githubusercontent.com/mcarans/hdx-scraper-unesco/master/tests/fixtures/EDU_FINANCE.csv')
+        content = r.content
+        r.close()
+        return content
 
     @pytest.fixture(scope='class')
     def endpoints_metadata(self):
         return {'EDU_FINANCE': ('Education: Financial resources',
                                 'http://yyyy/data/UNESCO,EDU_FINANCE/..........%s.?',
-                                'http://uis.unesco.org/en/topic/education-finance')}
+                                'http://uis.unesco.org/en/topic/education-finance',
+                                observations)}
 
     @pytest.fixture(scope='function')
-    def downloader(self):
+    def downloader(self, csv_content):
         class Response:
             @staticmethod
             def json():
@@ -50,6 +63,7 @@ class TestUnesco:
                         return {'structure': {'name': 'Education: Financial resources',
                                               'dimensions': {'observation': observations}}}
                     response.json = fn
+                    response.content = csv_content
                 return response
 
             @staticmethod
@@ -70,35 +84,32 @@ class TestUnesco:
         assert endpoints_metadata_actual == endpoints_metadata
 
     def test_generate_dataset_and_showcase(self, configuration, downloader, endpoints_metadata):
-        dataset, showcase = generate_dataset_and_showcase(downloader, countrydata, endpoints_metadata)
-        assert dataset == {'tags': [{'name': 'indicators'}, {'name': 'sustainable development'},
-                                    {'name': 'demographics'}, {'name': 'socioeconomics'}, {'name': 'education'}],
-                           'owner_org': '18f2d467-dcf8-4b7e-bffa-b3c338ba3a7c', 'data_update_frequency': '365',
-                           'title': 'Argentina - Sustainable development, Education, Demographic and Socioeconomic Indicators',
-                           'groups': [{'name': 'arg'}], 'maintainer': '196196be-6037-4488-8b71-d786adf4c081',
-                           'name': 'unesco-indicators-for-argentina', 'dataset_date': '01/01/1970-12/31/2014',
-                           'subnational': '0'}
-        resources = dataset.get_resources()
+        with temp_dir('UNESCO') as folder:
+            res = generate_dataset_and_showcase(downloader, countrydata, endpoints_metadata, folder=folder)
+            dataset, showcase = next(res)
+            assert dataset == {'tags': [{'name': 'sustainable development', 'vocabulary_id': '4e61d464-4943-4e97-973a-84673c1aaa87'},
+                                        {'name': 'demographics', 'vocabulary_id': '4e61d464-4943-4e97-973a-84673c1aaa87'},
+                                        {'name': 'socioeconomics', 'vocabulary_id': '4e61d464-4943-4e97-973a-84673c1aaa87'},
+                                        {'name': 'education', 'vocabulary_id': '4e61d464-4943-4e97-973a-84673c1aaa87'}],
+                               'owner_org': '18f2d467-dcf8-4b7e-bffa-b3c338ba3a7c', 'data_update_frequency': '365',
+                               'title': 'UNESCO Education: Financial resources - Argentina',
+                               'groups': [{'name': 'arg'}], 'maintainer': '196196be-6037-4488-8b71-d786adf4c081',
+                               'name': 'unesco-education-financial-resources-argentina', 'dataset_date': '01/01/1970-12/31/2014',
+                               'subnational': '0'}
+            resources = dataset.get_resources()
 
-        expected_description = 'To save, right click download button & click Save Link/Target As  \n[Info on Education: Financial resources](http://uis.unesco.org/en/topic/education-finance)'
-        assert resources == [{'description': expected_description,
-                              'url': 'http://yyyy/data/UNESCO,EDU_FINANCE/..........AR.?format=csv&startPeriod=2010&endPeriod=2014&locale=en&subscription-key=12345',
-                              'name': 'Education: Financial resources (2010-2014)', 'format': 'csv'},
-                             {'description': expected_description,
-                              'url': 'http://yyyy/data/UNESCO,EDU_FINANCE/..........AR.?format=csv&startPeriod=2005&endPeriod=2009&locale=en&subscription-key=12345',
-                              'name': 'Education: Financial resources (2005-2009)', 'format': 'csv'},
-                             {'description': expected_description,
-                              'url': 'http://yyyy/data/UNESCO,EDU_FINANCE/..........AR.?format=csv&startPeriod=1999&endPeriod=2004&locale=en&subscription-key=12345',
-                              'name': 'Education: Financial resources (1999-2004)', 'format': 'csv'},
-                             {'description': expected_description,
-                              'url': 'http://yyyy/data/UNESCO,EDU_FINANCE/..........AR.?format=csv&startPeriod=1970&endPeriod=1998&locale=en&subscription-key=12345',
-                              'name': 'Education: Financial resources (1970-1998)', 'format': 'csv'}]
+            assert resources == [{'description': 'Government expenditure per student', 'format': 'csv', 'name': 'XUNIT', 'resource_type': 'file.upload', 'url_type': 'upload'}]
 
-        assert showcase == {'name': 'unesco-indicators-for-argentina-showcase',
-                            'notes': 'Education, literacy and other indicators for Argentina',
-                            'image_url': 'http://www.tellmaps.com/uis/internal/assets/uisheader-en.png',
-                            'url': 'http://uis.unesco.org/en/country/AR',
-                            'tags': [{'name': 'indicators'}, {'name': 'sustainable development'},
-                                     {'name': 'demographics'}, {'name': 'socioeconomics'}, {'name': 'education'}],
-                            'title': 'Indicators for Argentina'}
+            assert showcase == {'name': 'unesco-education-financial-resources-argentina-showcase',
+                                'notes': 'Education, literacy and other indicators for Argentina',
+                                'image_url': 'http://www.tellmaps.com/uis/internal/assets/uisheader-en.png',
+                                'url': 'http://uis.unesco.org/en/country/AR',
+                                'tags': [{'name': 'sustainable development',
+                                          'vocabulary_id': '4e61d464-4943-4e97-973a-84673c1aaa87'},
+                                         {'name': 'demographics',
+                                          'vocabulary_id': '4e61d464-4943-4e97-973a-84673c1aaa87'},
+                                         {'name': 'socioeconomics',
+                                          'vocabulary_id': '4e61d464-4943-4e97-973a-84673c1aaa87'},
+                                         {'name': 'education', 'vocabulary_id': '4e61d464-4943-4e97-973a-84673c1aaa87'}],
+                                'title': 'UNESCO Education: Financial resources - Argentina'}
 
